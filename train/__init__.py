4069a69656d339a16c649438cf982e3b736f92f0
vihaan86 <51486593+vihaan86@users.noreply.github.com>
2026-02-24 11:14:09 +0530

    Delete train directory

--- train/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Training utilities for BERT MLM and NER."""
-
-from .data_ontonotes import get_tokenizer, get_ontonotes_dataloader
-from .data_ner import get_ner_dataloader, ONTONOTES_NUM_LABELS
-
-__all__ = ["get_tokenizer", "get_ontonotes_dataloader", "get_ner_dataloader", "ONTONOTES_NUM_LABELS"]
